3f44133b5244b01bafa85e41ccfd1fc96146e39e
Dolan Murphy <admin@dmur.me>
2014-03-07 14:46:52 -0800

    Uses JSONP to contact Echo Nest API

--- app/views/View.js
+++ app/views/View.js
@@ -28,8 +28,8 @@ define(['backbone', 'underscore', 'text!templates/view.html'], function(Backbone
 
             that.$('h1')
               .text(dj + " is playing some filthy jams");
-            that
-              .$('h2').text(artist + " - " + track);
+            that.$('h2')
+              .text(artist + " - " + track);
 
             that.loadArtistImage(artist);
           },
@@ -44,16 +44,19 @@ define(['backbone', 'underscore', 'text!templates/view.html'], function(Backbone
       loadArtistImage: function(artist) {
 
         var imageUrl = "http://developer.echonest.com/api/v4/artist/images?" +
-          "api_key=" + "UOWZEQZDWKY7GO4CO" + "&format=json" +
-          "&results=10" + "&start=0" + "&license=unknown";
+          "api_key=" + "UOWZEQZDWKY7GO4CO" + "&format=jsonp" +
+          "&results=10" + "&start=0" + "&license=unknown" + 
+          "&name=" + artist.split(' ').join('+');
 
         $.ajax({
-
+          type: 'GET',
+          async: 'false',
           url: imageUrl,
-
-          success: function(results) {
-            console.log(results);
-            //$('body').attr('background', )
+          dataType: 'jsonp',
+          jsonpCallback: 'success',
+          contentType: 'application/json',
+          success: function(json) {
+            console.log(json);
           },
 
           error: function(xhr, status, err) {
